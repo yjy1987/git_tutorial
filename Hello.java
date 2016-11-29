3afbed3985d748a0606984079143c9a8f9760dc2
Yeo JY <yeogandalf@gmail.com>
2016-11-29 11:16:50 +0900

    Hello his git

--- Hello.java
+++ Hello.java
@@ -1,1 +1,8 @@
-public class Hello{	public static void main(String[] args){		System.out.println("Hello Git");	}}
+public class Hello{	
+	public static void main(String[] args){		
+		System.out.println("Hello Git");
+		System.out.println("Hello his Git");
+
+
+
+}}
