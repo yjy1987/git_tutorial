328361f0609be2b9803e5ab99b2ba584a15d7d2b
Yeo JY <yeogandalf@gmail.com>
2016-11-29 11:11:00 +0900

    add output "Helloi her git"

--- Hello.java
+++ Hello.java
@@ -1,1 +1,4 @@
-public class Hello{	public static void main(String[] args){		System.out.println("Hello Git");	}}
+public class Hello{	public static void main(String[] args){		System.out.println("Hello Git");
+System.out.println("안녕하세요");
+System.out.println("이거 왤케 어렵냐");
+}}
